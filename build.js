29f25b60b4d801527dcf0242c9a6e12a7fb1237c
doup <asier@illarra.com>
2015-06-30 00:59:16 +0200

    Overhaul: separate plugins, mingo plugin created…

--- build.js
+++ build.js
@@ -2,7 +2,6 @@
 
 var Metalsmith   = require('metalsmith');
 var branch       = require('metalsmith-branch');
-var collections  = require('metalsmith-collections');
 var concat       = require('metalsmith-concat');
 var filenameDate = require('metalsmith-date-in-filename');
 var drafts       = require('metalsmith-drafts');
@@ -12,103 +11,18 @@ var ignore       = require('metalsmith-ignore');
 var pandoc       = require('metalsmith-pandoc');
 var permalinks   = require('metalsmith-permalinks');
 var sass         = require('metalsmith-sass');
+var slug         = require('metalsmith-slug');
 var snippet      = require('metalsmith-snippet');
 var templates    = require('metalsmith-templates');
 var uglify       = require('metalsmith-uglify');
 var wordcount    = require('metalsmith-word-count');
 
-// Plugin
-function multiLanguage(ops) {
-    var extname = require('path').extname;
-
-    var pattern = RegExp('.*_('+ ops.locales.join('|') +')(?:\..*)?$');
-    var file;
-
-    function getBaseFilename(file) {
-        var base = file;
-        var ext  = extname(base);
-
-        base = base.replace(RegExp('_('+ ops.locales.join('|') +')(?:'+ ext +')?$'), '_' + ops.default + ext);
-
-        return base;
-    }
-
-    function getAltFilename(file, fromLocale, toLocale) {
-        var ext = extname(file);
-
-        return file.replace('_'+ fromLocale + ext, '_'+ toLocale + ext);
-    }
-
-    function getLocale(file) {
-        return file.match(pattern)[1];
-    }
-
-    function merge(src, dest) {
-        for (var key in src) {
-            if (!dest.hasOwnProperty(key)) {
-                dest[key] = src[key];
-            }
-        }
-    }
-
-    return function (files, ms, done) {
-        ms.metadata().locales = ops;
-        ms.metadata().filterByLocale = function (arr, locale) {
-            return arr.filter(function (el, index) {
-                return el.locale == locale;
-            });
-        };
-
-        for (file in files) {
-            if (pattern.test(file)) {
-                var base = getBaseFilename(file);
-
-                files[file].locale = getLocale(file);
-
-                // Add missing properties from base locale
-                // This lets to have base some generic properties
-                // applied only in the 'default' locale, e.g.: template
-                if (base !== file) {
-                    merge(files[base], files[file]);
-                }
-            } else {
-                files[file].locale = ops.default;
-            }
-
-            files[file].altFiles = {};
-
-            ops.locales.forEach(function (locale) {
-                if (locale != files[file].locale) {
-                    files[file].altFiles[locale] = files[getAltFilename(file, files[file].locale, locale)];
-                } else {
-                    files[file].altFiles[files[file].locale] = files[file];
-                }
-            });
-        }
-
-        // Index handling
-        // Default locale will go in 'index.html'
-        // Other index-es in '/:locale/index.html'
-        for (file in files) {
-            if (/^index/.test(file)) {
-                var ext = extname(file);
-
-                if (files[file].locale == ops.default) {
-                    files[file].path = '';
-                    files['index'+ ext] = files[file];
-                } else {
-                    files[file].path = files[file].locale +'/';
-                    files[files[file].locale + '/index'+ext] = files[file];
-                }
-
-                // Remove old entry
-                delete files[file];
-            }
-        }
-
-        done();
-    };
-}
+// Custom
+var setProperty   = require('./plugins').setProperty;
+var i18n          = require('./plugins').i18n;
+var includeFiles  = require('./plugins').includeFiles;
+var mingo         = require('./plugins').mingo;
+var multiLanguage = require('./plugins').multiLanguage;
 
 function showDrafts(show) {
     return function (files, ms, done) {
@@ -122,81 +36,21 @@ function showDrafts(show) {
     };
 }
 
-// Plugin
-function i18n(ops) {
-    var i18n = require('i18n');
+function generateId(filename, filedata, ms) {
+    var locales = ms.metadata().locales.locales;
+    var ext     = require('path').extname(filename);
 
-    i18n.configure({
-        defaultLocale: ops.default,
-        locales:       ops.locales,
-        directory:     ops.directory
-    });
-
-    function __(str, data) {
-        return i18n.__({ phrase: str, locale: this.locale }, data || {});
-    }
-
-    return function (files, ms, done) {
-        for (var file in files) {
-            files[file].__ = __.bind(files[file]);
-        }
-
-        done();
-    };
-}
-
-function generateIds() {
-    var extname = require('path').extname;
-
-    function idFromFilename(file, locales) {
-        var ext = extname(file);
-
-        return file.replace('/', '_')
+    return filename.replace('/', '_')
                    .replace(ext, '')
                    .replace(/\d{8}\-/, '')
                    .replace(RegExp('_('+ locales.join('|') +')$'), '');
+}
+
+var viewHelpers = {
+    prettyDate: function (date) {
+        return date.toISOString().substr(0, 10);
     }
-
-    return function (files, ms, done) {
-        ms.metadata().ids = {};
-
-        for (var file in files) {
-            files[file].id = idFromFilename(file, ms.metadata().locales.locales);
-            ms.metadata().ids[files[file].id] = files[file];
-        }
-
-        ms.metadata().findById = function (id) {
-            return ms.metadata().ids[id];
-        };
-
-        done();
-    };
-}
-
-// Plugin
-function includeFiles(includes) {
-    var readFileSync = require('fs').readFileSync;
-    var path         = require('path');
-
-    return function (files, ms, done) {
-        includes.forEach(function (include) {
-            files[include] = { contents: readFileSync(path.join(__dirname, include)) }
-        });
-
-        done();
-    };
-}
-
-        done();
-    };
-}
-
-/*
-.use(function (files, ms, done) {
-    //console.log(files);
-    done();
-})
-*/
+};
 
 module.exports = function (isDebug, done) {
     var locale  = 'ca';
@@ -207,10 +61,11 @@ module.exports = function (isDebug, done) {
         'bower_components/foundation/js/foundation/foundation.js',
         'bower_components/foundation/js/foundation/foundation.reveal.js',
         'bower_components/unorphan/index.js',
-        'node_modules/hypher/dist/jquery.hypher.js'
+        'node_modules/hypher/dist/jquery.hypher.js',
     ];
 
     Metalsmith(__dirname)
+        .metadata(viewHelpers)
         .use(ignore(['.DS_Store', '*/.DS_Store', 'assets/images/*', 'templates/*', 'translations/*']))
 
         // Multi-language
@@ -237,9 +92,7 @@ module.exports = function (isDebug, done) {
             ]),
             output: 'assets/main.js'
         }))
-        .use(uglify({
-            removeOriginal: true
-        }))
+        .use(uglify({ removeOriginal: true }))
 
         // Add hash to CSS/JS filename for cache invalidation
         .use(fingerprint({ pattern: ['assets/main.css', 'assets/main.min.js'] }))
@@ -252,25 +105,20 @@ module.exports = function (isDebug, done) {
             locales:   locales,
             directory: 'src/translations'
         }))
-        .use(generateIds())
-        .use(collections({
-            articles: {
-                pattern: 'articles/*.md',
-                sortBy:  'date',
-                reverse: true
-            }
-        }))
+        .use(setProperty('id', generateId))
+        .use(slug({ patterns: ['*.md'], lower: true }))
         .use(pandoc())
         .use(snippet({ maxLength: 400 }))
         .use(wordcount({ raw: true }))
         .use(fileMetadata([
-            { pattern: 'articles/*', preserve: true, metadata: { template: 'article.jade', allow_comments: true } }
+            { pattern: 'articles/*', preserve: true, metadata: { template: 'article.jade', allow_comments: true, type: 'article' } }
         ]))
         .use(branch('articles/*')
             .use(permalinks({
                 pattern: ':locale/:slug'
             }))
         )
+        .use(mingo())
         .use(templates({
             engine:    'jade',
             directory: 'src/templates'
---
+++ plugins.js
@@ -0,0 +1,176 @@
+'use strict';
+
+module.exports.multiLanguage = function (ops) {
+    var extname = require('path').extname;
+
+    var pattern = RegExp('.*_('+ ops.locales.join('|') +')(?:\..*)?$');
+    var file;
+
+    function getBaseFilename(file) {
+        var base = file;
+        var ext  = extname(base);
+
+        base = base.replace(RegExp('_('+ ops.locales.join('|') +')(?:'+ ext +')?$'), '_' + ops.default + ext);
+
+        return base;
+    }
+
+    function getAltFilename(file, fromLocale, toLocale) {
+        var ext = extname(file);
+
+        return file.replace('_'+ fromLocale + ext, '_'+ toLocale + ext);
+    }
+
+    function getLocale(file) {
+        return file.match(pattern)[1];
+    }
+
+    function merge(src, dest) {
+        for (var key in src) {
+            if (!dest.hasOwnProperty(key)) {
+                dest[key] = src[key];
+            }
+        }
+    }
+
+    return function (files, ms, done) {
+        ms.metadata().locales = ops;
+        ms.metadata().filterByLocale = function (arr, locale) {
+            return arr.filter(function (el, index) {
+                return el.locale == locale;
+            });
+        };
+
+        for (file in files) {
+            if (pattern.test(file)) {
+                var base = getBaseFilename(file);
+
+                files[file].locale = getLocale(file);
+
+                // Add missing properties from base locale
+                // This lets to have base some generic properties
+                // applied only in the 'default' locale, e.g.: template
+                if (base !== file) {
+                    merge(files[base], files[file]);
+                }
+            } else {
+                files[file].locale = ops.default;
+            }
+
+            files[file].altFiles = {};
+
+            ops.locales.forEach(function (locale) {
+                if (locale != files[file].locale) {
+                    files[file].altFiles[locale] = files[getAltFilename(file, files[file].locale, locale)];
+                } else {
+                    files[file].altFiles[files[file].locale] = files[file];
+                }
+            });
+        }
+
+        // Index handling
+        // Default locale will go in 'index.html'
+        // Other index-es in '/:locale/index.html'
+        for (file in files) {
+            if (/^index/.test(file)) {
+                var ext = extname(file);
+
+                if (files[file].locale == ops.default) {
+                    files[file].path = '';
+                    files['index'+ ext] = files[file];
+                } else {
+                    files[file].path = files[file].locale +'/';
+                    files[files[file].locale + '/index'+ext] = files[file];
+                }
+
+                // Remove old entry
+                delete files[file];
+            }
+        }
+
+        done();
+    };
+}
+
+// Plugin
+module.exports.i18n = function (ops) {
+    var i18n = require('i18n');
+
+    function __(str, data) {
+        return i18n.__({ phrase: str, locale: this.locale }, data || {});
+    }
+
+    return function (files, ms, done) {
+        i18n.configure({
+            defaultLocale: ops.default,
+            locales:       ops.locales,
+            directory:     ms.path(ops.directory)
+        });
+
+        for (var file in files) {
+            files[file].__ = __.bind(files[file]);
+        }
+
+        done();
+    };
+}
+
+// Plugin
+module.exports.setProperty = function (prop, value) {
+    return function (files, ms, done) {
+        for (var file in files) {
+            if (typeof(value) === 'function') {
+                files[file][prop] = value(file, files[file], ms);
+            } else {
+                files[file][prop] = value;
+            }
+        }
+
+        done();
+    };
+}
+
+// Plugin
+module.exports.mingo = function () {
+    var Mingo = require('mingo');
+    var data  = [];
+    var metadata;
+
+    return function (files, ms, done) {
+        metadata = ms.metadata();
+
+        for (var file in files) {
+            files[file]._filename = file;
+            data.push(files[file]);
+        }
+
+        metadata.find = function (criteria, projection) {
+            return Mingo.find(data, criteria, projection);
+        };
+
+        metadata.findOne = function (criteria, projection) {
+            return Mingo.find(data, criteria, projection).first();
+        };
+
+        done();
+    };
+};
+
+// Plugin
+module.exports.includeFiles = function (includes) {
+    var glob         = require('glob');
+    var readFileSync = require('fs').readFileSync;
+    var relative     = require('path').relative;
+
+    return function (files, ms, done) {
+        var directory = ms.directory();
+
+        includes.forEach(function (include) {
+            glob.sync(ms.path(include)).forEach(function (match) {
+                files[relative(directory, match)] = { contents: readFileSync(match) };
+            });
+        });
+
+        done();
+    };
+}
