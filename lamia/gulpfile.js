bdc608ddd935c6d278be0be602ce6dd7c034c215
doup <asier@illarra.com>
2016-02-21 15:53:00 +0100

    Add deploy options

--- lamia/gulpfile.js
+++ lamia/gulpfile.js
@@ -4,4 +4,4 @@ var gulp = require('gulp');
 var Lamia = require('lamia');
 var path = require('path');
 
-gulp.registry(new Lamia(path.join(__dirname, '../')));
+gulp.registry(new Lamia(path.join(__dirname, '../'), require('./deploy.json')));
